961a6b5bb0f49e490b36acdab9273e4aa11d1f56
Stefan Walraven <stefan.walraven@cs.kuleuven.be>
2014-04-09 16:08:44 +0200

    test

--- csv/src/main/java/drm/taskworker/workers/CSVtoTaskWorker.java
+++ csv/src/main/java/drm/taskworker/workers/CSVtoTaskWorker.java
@@ -60,11 +60,11 @@ public class CSVtoTaskWorker extends Worker {
 			List<String[]> rows = parser.readAll();
 			String[] headers = rows.get(0);
 			int batchSize = 1;
-			try{ 
+//			try{ 
 				batchSize = Integer.valueOf(task.getJobOption("batch.size"));
-			} catch(Exception e) {
-				// keep batchSize == 1
-			}
+//			} catch(Exception e) {
+//				// keep batchSize == 1
+//			}
 
 			logger.info(String.format("Parsed %d records in CSV", rows.size() - 1));
 			int batchNb = 1;
@@ -79,7 +79,8 @@ public class CSVtoTaskWorker extends Worker {
 					for (int r = 0; r < row.length; r++) {
 						document.put(headers[r], row[r]);
 					}
-					newTask.addParam("Doc#"+d, document);
+					int docNb = d + 1;
+					newTask.addParam("Doc#"+docNb, document);
 				}
 				newTask.addParam("BatchNb", batchNb);
 				
